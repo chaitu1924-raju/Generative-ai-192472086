31ceaf9328e938f295d6043a2caba33c5709b3ac
chaitu1924-raju <chaitugokaraju59@gmail.com>
2026-07-27 21:46:50 +0530

    Delete exp-7-192472086.py

--- exp-7-192472086.py
+++
@@ -1,5 +0,0 @@
-from transformers import pipeline
-
-classifier = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
-result = classifier("Your text here")
-print(result)
