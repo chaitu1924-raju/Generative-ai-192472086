4b1e6a1f236ea8981fbb5d3dec9a494546abd3b8
chaitu1924-raju <chaitugokaraju59@gmail.com>
2026-07-27 21:46:22 +0530

    Delete exp-13-192472086.py

--- exp-13-192472086.py
+++
@@ -1,5 +0,0 @@
-from transformers import pipeline
-
-generator = pipeline("text-generation", model="gpt2")
-output = generator("The future of technology is", max_length=40, num_return_sequences=1)
-print(output[0]['generated_text'])
