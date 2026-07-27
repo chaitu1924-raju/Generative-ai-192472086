57b8f0f6a9b362966f6f6122b5aa01d4b55302e6
chaitu1924-raju <chaitugokaraju59@gmail.com>
2026-07-27 21:47:01 +0530

    Delete exp-9-192472086.py

--- exp-9-192472086.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-generator = pipeline("text-generation", model="gpt2")
-
-result = generator("Artificial Intelligence", max_length=30)
-print(result[0]["generated_text"])
