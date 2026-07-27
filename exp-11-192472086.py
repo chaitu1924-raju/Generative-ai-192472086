0d7ede6ad9cbceaf49f717dc2185731798c1299b
chaitu1924-raju <chaitugokaraju59@gmail.com>
2026-07-27 21:45:03 +0530

    Delete exp-11-192472086.py

--- exp-11-192472086.py
+++
@@ -1,6 +0,0 @@
-from transformers import BertTokenizer
-
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-sentence = "Machine learning models are powerful tools."
-tokens = tokenizer.tokenize(sentence)
-print(tokens)
